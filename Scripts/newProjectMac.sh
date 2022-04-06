1210599af4d821224f8158db8f76794b9c4a9f49
ClancyLight <clancylight@gmail.com>
2022-04-06 23:25:39 +1200

    add script for creating project in mac

---
+++ Scripts/newProjectMac.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+projname=$1
+
+# mkdir $projname
+
+echo "Creating project: $projname"
+
+cp -R ../ProjectTemplateSDL ../$projname
+
+ls ../$projname
+sed -i "" "s/templateApp/$projname/g" ../$projname/makefile
+
+cprojname="TemplateProgram"
+
+sed -i "" "s/$cprojname/$projname/g" ../$projname/src/main.c
+
+
+cd ../$projname
+
+make;make run; make clean
